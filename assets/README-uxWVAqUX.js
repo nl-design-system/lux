66ce339d5c9fda7d2ecd47651eb5e2b21f5adc45
remypar5 <remypar5@users.noreply.github.com>
2023-12-20 15:52:59 +0000

    Deploying to gh-pages from @ nl-design-system/lux@422054785964af4dc0093f0b35672bef34ed55df 🚀

---
+++ assets/README-uxWVAqUX.js
@@ -0,0 +1,4 @@
+const n=`<!-- @license CC0-1.0 -->
+
+# Example Button component
+`;export{n as r};
---
+++ assets/notice.stories-VYI5Pgom.js
@@ -0,0 +1,19 @@
+import{M as a,e as i}from"./chunk-HLWAVYOI-5W2qr2S7.js";import{j as e}from"./jsx-runtime-Q5kDp4vx.js";import{u as s}from"./index-BxL6228P.js";import"./iframe-lTtUwlWs.js";import"../sb-preview/runtime.js";import"./index-DogsOklH.js";import"./react-18-qZm_vjb7.js";import"./index-83LIMwj7.js";const u=`# Auteursrecht {auteur}
+
+Copyright (c) {jaar} {auteur}
+
+## Logo en huisstijl
+
+Op het huisstijl en logo zijn auteursrechten van toepassing. Het gebruik van logo en huisstijl is alleen toegestaan voor gebruik door {auteur}.
+
+Wanneer je een bewerking van de software wilt gebruiken voor andere doeleinden, mag je niet het logo van {auteur} gebruiken en je ontwerpt een eigen huisstijl.
+
+## Lettertype
+
+Lettertypes die worden gebruikt voor de huisstijl zijn niet allemaal gratis en open source. Let op dat bij gebruik van die bijgeleverde lettertypes je een (betaalde) licentie regelt. Pas anders de configuratie aan om minder of andere lettertypes te gebruiken.
+
+## Toestemming
+
+Wanneer je het logo of de huisstijl wilt gebruiken kun je voor toestemming contact opnemen met {contactpersoon}.
+`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Example/Toestemming voor gebruik"}),`
+`,e.jsx(i,{children:u})]})}function m(t={}){const{wrapper:r}=Object.assign({},s(),t.components);return r?e.jsx(r,{...t,children:e.jsx(o,{...t})}):o()}const p=()=>{throw new Error("Docs-only story")};p.parameters={docsOnly:!0};const n={title:"Example/Toestemming voor gebruik",tags:["stories-mdx"],includeStories:["__page"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:m};const x=["__page"];export{x as __namedExportsOrder,p as __page,n as default};
