4637bbc8dd4dde079ceaf133d5d8e38341b8284a
Jaap-Hein Wester <j.h.wester@setonix.nl>
2024-05-10 16:56:56 +0200

    Split `isObject` from `mergeDeep`, added Docs

--- utils/helpers/mergeDeep.cjs
+++ utils/helpers/mergeDeep.cjs
@@ -1,7 +1,13 @@
-const isObject = (variable) => {
-  return typeof variable === 'object' && !Array.isArray(variable) && variable !== null;
-};
+const isObject = require('./isObject.cjs');
 
+/**
+ * Merge two objects and returns a new combined object.
+ * Arrays within the objects will be concatenated.
+ *
+ * @param {object} baseObj Base object to merge
+ * @param {object} incomingObj Other object to merge
+ * @returns {object} Merged object
+ */
 const mergeDeep = (baseObj, incomingObj) =>
   Object.keys(incomingObj).reduce((mergedObj, key) => {
     if (isObject(baseObj[key]) && isObject(incomingObj[key])) {
