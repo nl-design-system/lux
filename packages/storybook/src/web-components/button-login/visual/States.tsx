d2bf6ed6a884c338370aff10237a183a105ad68d
Jaap-Hein Wester <j.h.wester@setonix.nl>
2024-05-21 16:07:33 +0200

    Test to trigger Chromatic

--- packages/storybook/src/web-components/button-login/visual/States.tsx
+++ packages/storybook/src/web-components/button-login/visual/States.tsx
@@ -2,7 +2,7 @@ import { LuxButtonLogin } from '@lux/web-components-react';
 
 export const VisualStates = () => (
   <>
-    <h4 className="lux-heading-4">Action States</h4>
+    <h4 className="lux-heading-4">TEST States</h4>
     <LuxButtonLogin agent="digid" label="Hover" class="force-state--hover"></LuxButtonLogin>
     <LuxButtonLogin agent="digid" label="Active" class="force-state--active"></LuxButtonLogin>
     <LuxButtonLogin agent="digid" label="Focus" class="force-state--focus"></LuxButtonLogin>
