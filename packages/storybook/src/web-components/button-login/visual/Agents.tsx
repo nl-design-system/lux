df4da6eaf2ce8eac949be33861ff9b1b3070dc4c
Jaap-Hein Wester <j.h.wester@setonix.nl>
2024-05-22 09:20:42 +0200

    Test to trigger Chromatic

--- packages/storybook/src/web-components/button-login/visual/Agents.tsx
+++ packages/storybook/src/web-components/button-login/visual/Agents.tsx
@@ -2,11 +2,10 @@ import { LuxButtonLogin } from '@lux/web-components-react';
 
 export const VisualAgents = () => (
   <>
-    <h4 className="lux-heading-4">Agents</h4>
+    <h4 className="lux-heading-4">007</h4>
     <LuxButtonLogin agent="digid"></LuxButtonLogin>
     <LuxButtonLogin agent="digid-machtigen"></LuxButtonLogin>
     <LuxButtonLogin agent="eherkenning"></LuxButtonLogin>
     <LuxButtonLogin agent="eidas"></LuxButtonLogin>
-    <LuxButtonLogin agent="unknown"></LuxButtonLogin>
   </>
 );
