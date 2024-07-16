493c17350fab90ee328d5ed03edc828d9112fc43
Remy Parzinski <rs.parzinski@gmail.com>
2024-07-16 11:48:54 +0200

    docs(design-tokens): add core font

---
+++ packages/storybook/src/design-tokens/niveaus/DesignTokenFontPreview.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import tokenToCssVar from '../../utils/tokenToCssVar';
+
+type Props = {
+  token: string;
+  value: string;
+};
+
+const getPropertyFromToken = (token: string): string => {
+  const matches = token.match(/(font-[^.]+)/i);
+
+  if (!matches || !matches.length) {
+    return '';
+  }
+
+  return matches[0];
+};
+
+const DesignTokenFontPreview = ({ token, value }: Props) => {
+  const bubbleElem = useRef<HTMLElement>(null);
+  const previewStyle: React.CSSProperties = {};
+  const property = getPropertyFromToken(token);
+  const valueStyle: React.CSSProperties = {
+    [property]: tokenToCssVar(token),
+  } as React.CSSProperties;
+
+  return (
+    <>
+      <td>
+        <span className="dt-preview dt-preview--font" ref={bubbleElem} style={previewStyle}>
+          {token}
+        </span>
+      </td>
+      <td>
+        <span className="dt-value dt-value--font" style={valueStyle}>
+          {value}
+        </span>
+      </td>
+    </>
+  );
+};
+
+export { DesignTokenFontPreview };
---
+++ packages/storybook/src/utils/tokenToCssVar.ts
@@ -0,0 +1,9 @@
+/**
+ * Convert a design token to a CSS Custom property, a.k.a. CSS Variable
+ *
+ * @param token Design token to convert
+ * @returns Design Token converted to CSS Custom Property
+ */
+const tokenToCssVar = (token: string): string => `var(--${token.replace(/\./g, '-')})`;
+
+export default tokenToCssVar;
