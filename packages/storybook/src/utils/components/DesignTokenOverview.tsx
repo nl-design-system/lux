fc8d59bd737dc213ef9371bf3e987dbafe6ba522
Remy Parzinski <rs.parzinski@gmail.com>
2024-07-23 15:43:38 +0200

    fix(storybook): linting issue

--- packages/storybook/src/utils/components/DesignTokenOverview.tsx
+++ packages/storybook/src/utils/components/DesignTokenOverview.tsx
@@ -1,4 +1,4 @@
-import React, { type PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 
 const DesignTokenOverview = ({ children }: PropsWithChildren<void>) => (
   <section className="lux-theme--logius-light">{children}</section>
