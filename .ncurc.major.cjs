db49042776e5d4b4867d5ce393a4da25c65d20d1
Remy Parzinski <rs.parzinski@gmail.com>
2024-08-06 16:34:06 +0200

    docs: add TODO comments linking to backlog story

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -2,6 +2,11 @@ const minorConfig = require('./.ncurc.minor.cjs');
 
 module.exports = {
   ...minorConfig,
-  reject: [...minorConfig.reject, 'eslint', 'eslint-plugin-json', 'style-dictionary'],
+  reject: [
+    ...minorConfig.reject,
+    'eslint',
+    'eslint-plugin-json',
+    'style-dictionary', // TODO LUX-333 remove when updated
+  ],
   target: 'latest',
 };
--- patches/README.md
+++ patches/README.md
@@ -1,1 +1,3 @@
+<!-- TODO LUX-333 remove patch when style-dictionary is updated -->
+
 - `@tokens-studio__sd-transforms@0.15.2.patch` De patch is nodig omdat deze uit gaat van een verkeerde versie van `style-dictionary`. Zodra we die updaten kunnen we deze patch verwijderen en `@tokens-studio/sd-transforms` ook updaten.
