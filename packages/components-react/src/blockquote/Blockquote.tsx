d6e0d0c6197e6a85a7bb6c6de6b8b1d86ac8ca1f
Wouter Gowdy <wouter.gowdy@capgemini.com>
2024-09-12 15:52:22 +0200

    Blockquote

---
+++ packages/components-react/src/blockquote/Blockquote.tsx
@@ -0,0 +1,8 @@
+import { type BlockquoteProps as UtrechtBlockquoteProps } from '@utrecht/component-library-react';
+import { Blockquote as UtrechtBlockquote } from '@utrecht/component-library-react/dist/css-module';
+
+import './blockquote.scss';
+
+export type LuxBlockquoteProps = Omit<UtrechtBlockquoteProps, 'appearance'> & {};
+
+export const LuxBlockquote = UtrechtBlockquote;
