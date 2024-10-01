2697352054fa0e6de2a9fb598b7f9c7cc1fe64e7
MrSkippy <MrSkippy@users.noreply.github.com>
2024-10-01 14:17:34 +0000

    Deploying to gh-pages from @ nl-design-system/lux@c4b1f2e5ffbd2b88d028c50fd4ecaad129de7662 🚀

---
+++ assets/spacing-tokens-fG3NXdRd.js
@@ -0,0 +1,79 @@
+import { j as jsxRuntimeExports } from "./jsx-runtime-DnIIMar6.js";
+import { useMDXComponents } from "./index-DXHhUSGJ.js";
+import { ae as Meta } from "./index-BRPnPxf2.js";
+import { D as DesignTokenOverview, e as DesignTokenSpacingPreview } from "./DesignTokenSpacingPreview-yVeFn0oZ.js";
+import "./index-aC1ZMUrs.js";
+import "./_commonjsHelpers-CcAunmGO.js";
+import "./iframe-_3vXBaQD.js";
+import "../sb-preview/runtime.js";
+import "./index-B63kQkss.js";
+import "./index-DLlB04eo.js";
+import "./index-DtSuK_9N.js";
+import "./index-BdOSk9or.js";
+import "./createVisualRegressionStory-CMDMpfq0.js";
+function _createMdxContent(props) {
+  const _components = {
+    h1: "h1",
+    ...useMDXComponents(),
+    ...props.components
+  };
+  return jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, {
+    children: [jsxRuntimeExports.jsx(Meta, {
+      title: "Design Tokens/Core/Spacing"
+    }), "\n", jsxRuntimeExports.jsx(_components.h1, {
+      id: "spacing-design-tokens",
+      children: "Spacing Design Tokens"
+    }), "\n", jsxRuntimeExports.jsx(DesignTokenOverview, {
+      children: jsxRuntimeExports.jsxs("table", {
+        children: [jsxRuntimeExports.jsx("thead", {
+          children: jsxRuntimeExports.jsxs("tr", {
+            children: [jsxRuntimeExports.jsx("th", {
+              children: "Token"
+            }), jsxRuntimeExports.jsx("th", {
+              children: "Waarde"
+            })]
+          })
+        }), jsxRuntimeExports.jsxs("tbody", {
+          children: [jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.0"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.25"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.50"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.100"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.150"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.200"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.300"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.400"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.500"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.600"
+          }), jsxRuntimeExports.jsx(DesignTokenSpacingPreview, {
+            token: "lux.space.700"
+          })]
+        })]
+      })
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const { wrapper: MDXLayout } = {
+    ...useMDXComponents(),
+    ...props.components
+  };
+  return MDXLayout ? jsxRuntimeExports.jsx(MDXLayout, {
+    ...props,
+    children: jsxRuntimeExports.jsx(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+export {
+  MDXContent as default
+};
--- assets/testid-4ea5c11d-C8OEyvo2.js
+++
@@ -1,7 +0,0 @@
-import "./preview-CR_P2OSo.js";
-const applyTestId = (id) => {
-  return null;
-};
-export {
-  applyTestId as a
-};
