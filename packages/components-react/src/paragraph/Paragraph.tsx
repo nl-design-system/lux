619148fc3f351e6de17d725c841f9c05b6bbb38b
Jaap-Hein Wester <j.h.wester@setonix.nl>
2024-10-07 12:41:00 +0200

    Added `appearance` because Utrecht changes are not on the radar yet.

--- packages/components-react/src/paragraph/Paragraph.tsx
+++ packages/components-react/src/paragraph/Paragraph.tsx
@@ -1,1 +1,17 @@
-export { Paragraph, type ParagraphProps } from '@utrecht/component-library-react';
+import {
+  Paragraph as UtrechtParagraph,
+  type ParagraphProps as UtrechtParagraphProps,
+} from '@utrecht/component-library-react';
+
+export type ParagraphProps = Omit<UtrechtParagraphProps, 'lead' | 'small'> & {
+  appearance?: 'lead' | 'small';
+};
+
+export const Paragraph = (props: ParagraphProps) => {
+  const { appearance, ...restProps } = props;
+
+  const lead: boolean = appearance === 'lead';
+  const small: boolean = appearance === 'small';
+
+  return <UtrechtParagraph {...restProps} lead={lead} small={small} />;
+};
--- packages/storybook/src/react-components/paragraph/paragraph.stories.tsx
+++ packages/storybook/src/react-components/paragraph/paragraph.stories.tsx
@@ -1,23 +1,40 @@
 import { Paragraph } from '@lux-design-system/components-react';
 import type { Meta, StoryObj } from '@storybook/react';
-// import { type HTMLAttributes, type PropsWithChildren } from 'react';
-
-const LuxParagraph = ({ children }: any) => <Paragraph>{children}</Paragraph>;
 
 type Story = StoryObj<typeof meta>;
 
 const meta = {
   title: 'React Components/Paragraph',
   id: 'react-components-paragraph',
-  component: LuxParagraph,
-} satisfies Meta<typeof LuxParagraph>;
+  component: Paragraph,
+  argTypes: {
+    children: {
+      name: 'content (children)',
+      description: 'Paragraph text',
+      control: 'text',
+      table: {
+        type: {
+          summary: 'HTML Content',
+        },
+      },
+    },
+    appearance: {
+      name: 'appearance',
+      description: 'Appearance modifier',
+      control: 'select',
+      options: [undefined, 'lead', 'small'],
+    },
+  },
+} satisfies Meta<typeof Paragraph>;
 
 export default meta;
 
 export const Playground: Story = {
   name: 'Playground',
   args: {
-    children: 'lalala',
+    appearance: undefined,
+    children:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Dolor ante id varius, aenean eu faucibus vitae malesuada. Viverra malesuada aliquam et placerat justo porta ipsum parturient. Cursus nostra varius efficitur lobortis aliquam lectus bibendum.',
   },
   parameters: {
     docs: {
@@ -25,4 +42,35 @@ export const Playground: Story = {
     },
   },
   tags: ['!autodocs'],
+};
+
+export const Lead: Story = {
+  name: 'Lead',
+  args: {
+    ...Playground.args,
+    appearance: 'lead',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'De `lead` styling wordt toegepast. De content wordt in een `<b>` geplaatst t.b.v. visueel verschil als CSS niet geladen kan worden.',
+      },
+    },
+  },
+};
+
+export const Small: Story = {
+  name: 'Small',
+  args: {
+    ...Playground.args,
+    appearance: 'small',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'De `small` styling wordt toegepast. De content wordt in een `<small>` geplaatst.',
+      },
+    },
+  },
 };
