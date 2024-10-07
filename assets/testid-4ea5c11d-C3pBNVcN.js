e5747d00f819cbabb11db8b489c9cda0bbfb3ac0
AlineNap <59806622+AlineNap@users.noreply.github.com>
2024-10-07 12:47:49 +0000

    Deploying to gh-pages from @ nl-design-system/lux@6e97df6a175fb08349212a0cf54ef8be4754c297 🚀

---
+++ assets/testid-4ea5c11d-C3pBNVcN.js
@@ -0,0 +1,7 @@
+import "./preview-FxtPAxW_.js";
+const applyTestId = (id) => {
+  return null;
+};
+export {
+  applyTestId as a
+};
---
+++ assets/utrecht-icon-warning.entry-d58135a7-nLavcA7M.js
@@ -0,0 +1,19 @@
+import { a as registerInstance, b as h } from "./preview-FxtPAxW_.js";
+import "./iframe-DVJB8KU2.js";
+import "../sb-preview/runtime.js";
+import "./index-aC1ZMUrs.js";
+import "./_commonjsHelpers-CcAunmGO.js";
+const generatedCss = ":host{block-size:var(--utrecht-icon-size);color:var(--utrecht-icon-color);display:inline-block;font-size:var(--utrecht-icon-size);inline-size:var(--utrecht-icon-size);inset-block-start:var(--utrecht-icon-inset-block-start, 0);position:relative}svg{height:100%;pointer-events:none;width:100%}";
+const UtrechtIconWarningStyle0 = generatedCss;
+const UtrechtIconWarning = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  render() {
+    return h("svg", { key: "e773b4d072ea4ef429d05de02a72a8acd1f1d8af", viewBox: "-7 -8 38 38", xmlns: "http://www.w3.org/2000/svg", fill: "currentColor", height: "1em", width: "1em" }, h("path", { key: "6d0c9ecff36627c82cfdc2266aa5c3e3b33bdead", d: "M11.134 3.5a1 1 0 0 1 1.732 0L22.392 20a1 1 0 0 1-.866 1.5H2.474a1 1 0 0 1-.866-1.5z" }), h("rect", { key: "9e3485ae1b9d63f8f8acdaeaf0f40588ab679dd0", width: "1.9", height: "6.65", x: "12.9", y: "15.65", rx: ".5", transform: "rotate(-180 12.9 15.65)" }), h("circle", { key: "5ad8197ad57d602d57feb780a4d31c5933398649", cx: "11.95", cy: "17.55", r: ".95", transform: "rotate(-180 11.95 17.55)" }));
+  }
+};
+UtrechtIconWarning.style = UtrechtIconWarningStyle0;
+export {
+  UtrechtIconWarning as utrecht_icon_warning
+};
