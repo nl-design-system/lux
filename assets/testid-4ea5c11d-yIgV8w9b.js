ed8a77f5aae4eba908f64f43a28e075b9c26ec6f
AlineNap <59806622+AlineNap@users.noreply.github.com>
2024-10-14 10:08:11 +0000

    Deploying to gh-pages from @ nl-design-system/lux@eaf684ab2f24753d7afb49604b72afc4ab88ed1e 🚀

---
+++ assets/testid-4ea5c11d-yIgV8w9b.js
@@ -0,0 +1,7 @@
+import "./preview-BeL7q65L.js";
+const applyTestId = (id) => {
+  return null;
+};
+export {
+  applyTestId as a
+};
---
+++ assets/utrecht-icon-bewijsstukken.entry-605b55ac-C3mT8Hh4.js
@@ -0,0 +1,21 @@
+import { a as registerInstance, b as h } from "./preview-BeL7q65L.js";
+import "./jsx-runtime-DnIIMar6.js";
+import "./index-aC1ZMUrs.js";
+import "./_commonjsHelpers-CcAunmGO.js";
+import "./index-BwzAXmul.js";
+import "./iframe-t1uDzmrr.js";
+import "../sb-preview/runtime.js";
+const generatedCss = ":host{block-size:var(--utrecht-icon-size);color:var(--utrecht-icon-color);display:inline-block;font-size:var(--utrecht-icon-size);inline-size:var(--utrecht-icon-size);inset-block-start:var(--utrecht-icon-inset-block-start, 0);position:relative}svg{height:100%;pointer-events:none;width:100%}";
+const UtrechtIconBewijsstukkenStyle0 = generatedCss;
+const UtrechtIconBewijsstukken = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  render() {
+    return h("svg", { key: "7e100f275c15bbed734fdc84ca1521a44b12daf1", viewBox: "0 0 36 36", xmlns: "http://www.w3.org/2000/svg", fill: "currentColor", height: "1em", width: "1em" }, h("path", { key: "0073c45d30d998b9c0b7358d71faa80ae9740fc4", d: "M4 25.98h9v8.99zm24-8.99c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5m-7 5c0 1.9.76 3.62 2 4.89v9.13l-8-.02V26a2 2 0 0 0-2-2H3V1.97C3 .88 3.88 0 4.98 0h22.05c1.11 0 2.02.9 2.02 2.02l-.03 13.06c-.33-.05-.67-.08-1.02-.08-3.86 0-7 3.14-7 7Zm3-10.02c0-.54-.44-.98-.98-.98H9c-.55 0-1 .45-1 1s.45 1 1 1h14.02c.54 0 .98-.44.98-.98v-.05ZM7.99 7c0 .55.45.99 1 .99h14.03c.54 0 .98-.44.98-.98s-.44-.97-.97-.98L9 5.99c-.56 0-1.01.45-1 1.01ZM17 16.98c0-.55-.45-1-1-1H9c-.55 0-1 .45-1 1s.45 1 1 1h7c.55 0 1-.45 1-1m8 11.31v7.14c0 .43.51.66.83.37L28 33.87l2.17 1.93c.32.29.83.06.83-.37v-7.14c-.91.43-1.92.68-3 .68s-2.09-.25-3-.68" }));
+  }
+};
+UtrechtIconBewijsstukken.style = UtrechtIconBewijsstukkenStyle0;
+export {
+  UtrechtIconBewijsstukken as utrecht_icon_bewijsstukken
+};
