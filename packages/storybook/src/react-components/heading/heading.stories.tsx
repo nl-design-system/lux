7d229d12fb4b8bc267642e8c0f0f16880978302a
Jaap-Hein Wester <j.h.wester@setonix.nl>
2024-10-18 15:47:30 +0200

    Visual regression added to Headings

--- packages/storybook/src/react-components/heading/heading.stories.tsx
+++ packages/storybook/src/react-components/heading/heading.stories.tsx
@@ -6,11 +6,12 @@ import {
   LuxHeading4,
   LuxHeading5,
   LuxHeading6,
+  type LuxHeadingProps,
 } from '@lux-design-system/components-react';
 import tokens from '@lux-design-system/design-tokens/dist/index.json';
 import type { Meta, StoryObj } from '@storybook/react';
 import tokensDefinition from '@utrecht/heading-css/src/tokens.json';
-import { createDesignTokensStory } from '../../utils';
+import { createDesignTokensStory, createVisualRegressionStory, VisualRegressionWrapper } from '../../utils';
 
 type Story = StoryObj<typeof meta>;
 
@@ -96,3 +97,44 @@ export const HeadingWithDifferentAppearance: Story = {
 };
 
 export const DesignTokens = createDesignTokensStory(meta);
+
+const headings = ['indefinitus', 'primus', 'secundus', 'tertius', 'quārtus', 'quīntus', 'sextus', 'invalidus'];
+
+export const Visual = createVisualRegressionStory(() => (
+  <>
+    <h4 className="utrecht-heading-3">Light</h4>
+    <VisualRegressionWrapper className={`lux-theme--logius-light`} key={'light'}>
+      {headings.map((heading, index) => (
+        <>
+          <LuxHeading level={index as LuxHeadingProps['level']} key={'light-level-' + index}>
+            Lorem ipsum dolor sit amet, titulus {heading}.
+          </LuxHeading>
+          <LuxHeading
+            level={99 as LuxHeadingProps['level']}
+            appearance={index as LuxHeadingProps['appearance']}
+            key={'light-appeareance-' + index}
+          >
+            Lorem ipsum dolor sit amet, aspectus {heading}.
+          </LuxHeading>
+        </>
+      ))}
+    </VisualRegressionWrapper>
+    <h4 className="utrecht-heading-3">Dark</h4>
+    <VisualRegressionWrapper className={`lux-theme--logius-dark`} key="dark">
+      {headings.map((heading, index) => (
+        <>
+          <LuxHeading level={index as LuxHeadingProps['level']} key={'dark-level-' + index}>
+            Lorem ipsum dolor sit amet, titulus {heading}.
+          </LuxHeading>
+          <LuxHeading
+            level={99 as LuxHeadingProps['level']}
+            appearance={index as LuxHeadingProps['appearance']}
+            key={'dark-appearance-' + index}
+          >
+            Lorem ipsum dolor sit amet, aspectus {heading}.
+          </LuxHeading>
+        </>
+      ))}
+    </VisualRegressionWrapper>
+  </>
+));
