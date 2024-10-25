ad9c7ad2d5debb87e6199fe18b52972e871224d6
remypar5 <remypar5@users.noreply.github.com>
2024-10-25 09:52:15 +0000

    Deploying to gh-pages from @ nl-design-system/lux@e56d6bcae9e3769633b437ea462849810e049ebd 🚀

---
+++ assets/testid-4ea5c11d-CLUWl_Rg.js
@@ -0,0 +1,7 @@
+import "./preview-BMeRwzjS.js";
+const applyTestId = (id) => {
+  return null;
+};
+export {
+  applyTestId as a
+};
--- assets/testid-4ea5c11d-_aB79oDV.js
+++
@@ -1,7 +0,0 @@
-import "./preview-Pg0VBgQn.js";
-const applyTestId = (id) => {
-  return null;
-};
-export {
-  applyTestId as a
-};
