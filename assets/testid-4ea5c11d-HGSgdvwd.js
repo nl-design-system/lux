fb96d28275f8f1fa2d2fa8bf18901328943659a8
MrSkippy <MrSkippy@users.noreply.github.com>
2024-11-06 10:08:09 +0000

    Deploying to gh-pages from @ nl-design-system/lux@6de5a79be4a95007bd5627d92f5638a3f5f285db 🚀

---
+++ assets/testid-4ea5c11d-HGSgdvwd.js
@@ -0,0 +1,7 @@
+import "./preview-DxLg8yak.js";
+const applyTestId = (id) => {
+  return null;
+};
+export {
+  applyTestId as a
+};
---
+++ assets/utrecht-icon-schild-gemeente-utrecht.entry-a8e17d50-CeC_VBQ0.js
@@ -0,0 +1,21 @@
+import { a as registerInstance, b as h } from "./preview-DxLg8yak.js";
+import "./jsx-runtime-DnIIMar6.js";
+import "./index-aC1ZMUrs.js";
+import "./_commonjsHelpers-CcAunmGO.js";
+import "./index-pmKOP4ie.js";
+import "./iframe-Bj7hJXyZ.js";
+import "../sb-preview/runtime.js";
+const generatedCss = ":host{block-size:var(--utrecht-icon-size);color:var(--utrecht-icon-color);display:inline-block;font-size:var(--utrecht-icon-size);inline-size:var(--utrecht-icon-size);inset-block-start:var(--utrecht-icon-inset-block-start, 0);position:relative}svg{height:100%;pointer-events:none;width:100%}";
+const UtrechtIconSchildGemeenteUtrechtStyle0 = generatedCss;
+const UtrechtIconSchildGemeenteUtrecht = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  render() {
+    return h("svg", { key: "6c781e4b10619c5d4fd77c25b07f08308a78ae92", viewBox: "0 0 36 36", xmlns: "http://www.w3.org/2000/svg", fill: "currentColor", height: "1em", width: "1em" }, h("path", { key: "4ace5726bc7b5f1771d7e73882afc4d1d285cd4b", d: "M2 0v23.63c0 2.6.8 5.07 2.17 6.56s2.4 2.6 4.57 3.71c1.83.87 4.46 2.1 9.14 2.1s7.31-1.11 9.14-2.1c1.94-.99 2.97-1.98 4-2.97 1.71-1.61 2.97-4.33 2.97-7.3V0zm30 23c0 3-1.57 5.66-3 7L4 2h28z" }));
+  }
+};
+UtrechtIconSchildGemeenteUtrecht.style = UtrechtIconSchildGemeenteUtrechtStyle0;
+export {
+  UtrechtIconSchildGemeenteUtrecht as utrecht_icon_schild_gemeente_utrecht
+};
