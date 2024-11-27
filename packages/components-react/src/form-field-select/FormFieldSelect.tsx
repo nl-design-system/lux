90370e772134a8455136fe1af7278fcc028faabe
MMeijerink <github@markmeijerink.nl>
2024-11-27 15:44:10 +0100

    React components/select (#329)
    # Contents
    
    Nieuw component: Select en FormFieldSelect
    
    ---------
    
    Co-authored-by: Remy Parzinski <rs.parzinski@gmail.com>
    Co-authored-by: Jaap-Hein Wester <MrSkippy@users.noreply.github.com>
    Co-authored-by: Jaap-Hein Wester <j.h.wester@setonix.nl>

---
+++ packages/components-react/src/form-field-select/FormFieldSelect.tsx
@@ -0,0 +1,123 @@
+import { FormFieldProps as UtrechtFormFieldProps } from '@utrecht/component-library-react/dist/css-module';
+import clsx from 'clsx';
+import { Ref, useId } from 'react';
+import { LuxFormField } from '../form-field/FormField';
+import {
+  LuxFormFieldDescription,
+  type LuxFormFieldDescriptionAppearance,
+} from '../form-field-description/FormFieldDescription';
+import { LuxFormFieldErrorMessage } from '../form-field-error-message/FormFieldErrorMessage';
+import { LuxFormFieldLabel } from '../form-field-label/FormFieldLabel';
+import { LuxSelect, type LuxSelectProps } from '../select/Select';
+
+export interface LuxFormFieldSelectProps
+  extends Omit<UtrechtFormFieldProps, 'type' | 'onBlur' | 'onChange' | 'onFocus'>,
+    Pick<
+      LuxSelectProps,
+      | 'defaultValue'
+      | 'busy'
+      | 'className'
+      | 'disabled'
+      | 'invalid'
+      | 'name'
+      | 'onBlur'
+      | 'onChange'
+      | 'onFocus'
+      | 'required'
+      | 'size'
+      | 'value'
+    > {
+  appearance?: LuxFormFieldDescriptionAppearance;
+  distanced?: boolean;
+  inputRef?: Ref<HTMLSelectElement>;
+}
+
+export const LuxFormFieldSelect = ({
+  label,
+  description,
+  errorMessage,
+  disabled,
+  invalid,
+  appearance,
+  distanced,
+  className,
+  inputRef,
+  ...restProps
+}: LuxFormFieldSelectProps) => {
+  const inputId = useId();
+  const descriptionId = useId();
+  const errorMessageId = useId();
+
+  const labelNode =
+    typeof label === 'string' ? (
+      <LuxFormFieldLabel disabled={disabled} htmlFor={inputId}>
+        {label}
+      </LuxFormFieldLabel>
+    ) : (
+      label
+    );
+  const descriptionNode =
+    typeof description === 'string' ? (
+      <LuxFormFieldDescription appearance={invalid ? 'invalid' : appearance} id={descriptionId}>
+        {description}
+      </LuxFormFieldDescription>
+    ) : (
+      description
+    );
+  const errorMessageNode =
+    typeof errorMessage === 'string' ? (
+      <LuxFormFieldErrorMessage distanced={distanced} id={errorMessageId}>
+        {errorMessage}
+      </LuxFormFieldErrorMessage>
+    ) : (
+      errorMessage
+    );
+
+  // TODO: naar utils
+  function pick<T extends object, U extends keyof T>(obj: T, paths: Array<U>): Pick<T, U> {
+    const ret = {} as Pick<T, U>;
+    for (const k of paths) {
+      ret[k] = obj[k];
+    }
+    return ret;
+  }
+
+  const selectAttrs = pick(restProps, [
+    'busy',
+    'defaultValue',
+    'name',
+    'onBlur',
+    'onChange',
+    'onFocus',
+    'required',
+    'size',
+    'value',
+  ]);
+
+  const formFieldAttrs = pick(restProps, ['children']);
+
+  return (
+    <LuxFormField
+      label={labelNode}
+      description={descriptionNode}
+      errorMessage={errorMessageNode}
+      input={
+        <LuxSelect
+          ref={inputRef}
+          id={inputId}
+          invalid={invalid}
+          disabled={disabled}
+          aria-describedby={
+            clsx({
+              [descriptionId]: description,
+              [errorMessageId]: invalid,
+            }) || undefined
+          }
+          {...selectAttrs}
+        />
+      }
+      className={className}
+      {...formFieldAttrs}
+    />
+  );
+};
