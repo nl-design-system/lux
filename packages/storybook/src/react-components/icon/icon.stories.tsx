f97fdffd9878be467c15bd0ecf3b3adf61d4f91e
Jaap-Hein Wester <j.h.wester@setonix.nl>
2024-12-30 15:10:52 +0100

    fix typescript

--- packages/storybook/src/react-components/icon/icon.stories.tsx
+++ packages/storybook/src/react-components/icon/icon.stories.tsx
@@ -1,6 +1,6 @@
-import { Icon } from '@lux-design-system/components-react';
+import { Icon, IconProps } from '@lux-design-system/components-react';
 import { IconId, iconIds } from '@lux-design-system/icons';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { ArgTypes, Meta, StoryObj } from '@storybook/react';
 import * as tablerIcons from '@tabler/icons-react';
 import { ReactNode } from 'react';
 
@@ -14,17 +14,17 @@ const CustomLibrary: Partial<Record<string, ReactNode>> = {
 };
 
 interface ExtraArgs {
-  size: number;
+  size?: number | string;
 }
 
-type Story = StoryObj<typeof meta & ExtraArgs>;
+type Story = StoryObj<typeof meta> & { args: ExtraArgs };
 
 const meta = {
   title: 'React Components/Icon',
   id: 'react-components-icon',
   component: Icon,
   decorators: [
-    (Story, { args }) => (
+    (Story, { args }: { args: IconProps & ExtraArgs }) => (
       <>
         <style>
           {`:root {
@@ -48,7 +48,7 @@ const meta = {
     },
   },
   parameters: {},
-} satisfies Meta<typeof Icon & ExtraArgs>;
+} satisfies Meta<typeof Icon> & { args: ExtraArgs; argTypes: ArgTypes<ExtraArgs> };
 
 export default meta;
 
