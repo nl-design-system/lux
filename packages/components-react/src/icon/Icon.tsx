d15e4899543b4190aecf33b7a66f10e5261ce464
Jaap-Hein Wester <j.h.wester@setonix.nl>
2024-12-30 16:15:28 +0100

    clean up

--- packages/components-react/src/icon/Icon.tsx
+++ packages/components-react/src/icon/Icon.tsx
@@ -1,16 +1,15 @@
 import IconSet, { type IconId } from '@lux-design-system/icons';
-// import * as IconsTabler from '@tabler/icons-react';
 import { Icon as UtrechtIcon, type IconProps as UtrechtIconProps } from '@utrecht/component-library-react';
 import { PropsWithChildren, ReactNode } from 'react';
 import FallbackIcon from './FallbackIcon';
 
 export type LuxIconProps = UtrechtIconProps & {
   iconId: IconId | string;
-  library?: Partial<Record<string, ReactNode>>;
+  library?: Record<string, ReactNode>;
 };
 
 export const LuxIcon = ({ iconId, library, children }: PropsWithChildren<LuxIconProps>): ReactNode => {
-  const iconSet: Partial<Record<string, ReactNode>> = library || IconSet;
+  const iconSet: Record<string, ReactNode> = library || IconSet;
 
   return (
     <UtrechtIcon>
