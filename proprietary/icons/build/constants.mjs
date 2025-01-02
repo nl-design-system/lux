32009133f67dc2eaf6d354dfa5eecc723508d0cb
Jaap-Hein Wester <MrSkippy@users.noreply.github.com>
2025-01-02 11:06:04 +0100

    feat(components-react): add Icon component and Icons package (#349)

---
+++ proprietary/icons/build/constants.mjs
@@ -0,0 +1,4 @@
+export const SRC_FOLDER = './src';
+export const DIST_FOLDER = './dist';
+export const IMPORTED_SRC_FOLDER = `${SRC_FOLDER}/imported`;
+export const REACT_ICONS_FOLDER = `${DIST_FOLDER}/react-icons`;
