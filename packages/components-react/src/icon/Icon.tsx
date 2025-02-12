dab1672742a237247293c26a85fa4fd66b9b7025
Raoul Wittenberns <raoul.wittenberns@logius.nl>
2025-02-12 12:58:18 +0100

    Remove direct link to proprietary icon package

--- packages/components-react/src/icon/Icon.tsx
+++ packages/components-react/src/icon/Icon.tsx
@@ -1,4 +1,3 @@
-import IconSet, { type IconId } from '@lux-design-system/icons';
 import {
   Icon as UtrechtIcon,
   type IconProps as UtrechtIconProps,
@@ -7,21 +6,19 @@ import { PropsWithChildren, ReactNode } from 'react';
 import FallbackIcon from './FallbackIcon';
 
 export type LuxIconProps = UtrechtIconProps & {
-  iconId: IconId | string;
+  iconId: string;
   library?: Record<string, ReactNode>;
 };
 
 export const LuxIcon = ({ iconId, library, children }: PropsWithChildren<LuxIconProps>): ReactNode => {
-  const iconSet: Record<string, ReactNode> = library || IconSet;
-
   return (
     <UtrechtIcon>
       <style>
         {`svg {
-  fill: currentColor;          
+  fill: currentColor;
           }`}
       </style>
-      {iconId in iconSet ? iconSet[iconId] : <FallbackIcon />}
+      {library !== undefined && iconId in library ? library[iconId] : <FallbackIcon />}
       {children}
     </UtrechtIcon>
   );
