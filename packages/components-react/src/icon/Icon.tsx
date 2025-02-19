6fa37afa90d798ac24c21550f336cababb634dc5
Raoul <85627716+rwittenberns@users.noreply.github.com>
2025-02-19 09:11:57 +0100

    Icon optimizations (#401)
    # Contents
    
    <!-- Description of what this PR contains. New components/features,
    bugfixes, etc. -->
    
    ## Checklist
    
    <!-- Surround an item with double tildes `~~` to indicate that it does
    not apply to this PR -->
    
    - [ ] New features/components and bugfixes are covered by tests
    - [ ] Changesets are created
    - [ ] Definition of Done is checked
    
    ---------
    
    Co-authored-by: Dinesh Duggal <dinesh.duggal@koop.overheid.nl>

--- packages/components-react/src/icon/Icon.tsx
+++ packages/components-react/src/icon/Icon.tsx
@@ -1,24 +1,23 @@
-import IconSet, { type IconId } from '@lux-design-system/icons';
-import { Icon as UtrechtIcon, type IconProps as UtrechtIconProps } from '@utrecht/component-library-react';
+import {
+  Icon as UtrechtIcon,
+  type IconProps as UtrechtIconProps,
+} from '@utrecht/component-library-react/dist/css-module';
 import { PropsWithChildren, ReactNode } from 'react';
 import FallbackIcon from './FallbackIcon';
 
 export type LuxIconProps = UtrechtIconProps & {
-  iconId: IconId | string;
-  library?: Record<string, ReactNode>;
+  icon?: ReactNode;
 };
 
-export const LuxIcon = ({ iconId, library, children }: PropsWithChildren<LuxIconProps>): ReactNode => {
-  const iconSet: Record<string, ReactNode> = library || IconSet;
-
+export const LuxIcon = ({ icon, children }: PropsWithChildren<LuxIconProps>): ReactNode => {
   return (
     <UtrechtIcon>
       <style>
         {`svg {
-  fill: currentColor;          
+  fill: currentColor;
           }`}
       </style>
-      {iconId in iconSet ? iconSet[iconId] : <FallbackIcon />}
+      {icon !== undefined ? icon : <FallbackIcon />}
       {children}
     </UtrechtIcon>
   );
