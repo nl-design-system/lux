0b6f9764c9c6febeb34a3e3b6312c488695100aa
Dinesh Duggal <dinesh.duggal@koop.overheid.nl>
2025-02-20 14:33:31 +0100

    feat: configured KOOP theme

--- packages/storybook/config/themes.ts
+++ packages/storybook/config/themes.ts
@@ -7,4 +7,5 @@ import '@lux-design-system/design-tokens/dist/digid/index-theme.css';
 import '@lux-design-system/design-tokens/dist/logius/index-theme.css';
 import '@lux-design-system/design-tokens/dist/eva/index-theme.css';
 import '@lux-design-system/design-tokens/dist/mijnoverheid/index-theme.css';
+import '@lux-design-system/design-tokens/dist/koop/index-theme.css';
 import '@lux-design-system/design-tokens/dist/nldoc/index-theme.css';
