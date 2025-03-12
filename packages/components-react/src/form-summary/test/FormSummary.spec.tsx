443dd2e162e1bc0cc0c9ac8c217cf982037c7e26
edblocker <eddy.harskamp@hybrit.org>
2025-03-12 09:52:30 +0100

    feat(370): Remove unused import in FormSummary test

--- packages/components-react/src/form-summary/test/FormSummary.spec.tsx
+++ packages/components-react/src/form-summary/test/FormSummary.spec.tsx
@@ -1,6 +1,5 @@
 import { describe, expect, it } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
-import * as React from 'react';
 import {
   LuxFormSummary,
   LuxFormSummaryActions,
