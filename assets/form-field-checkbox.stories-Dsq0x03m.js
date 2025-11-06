e94b5cf58896c1ccf2f906af77dad9f2df40a843
rwittenberns <85627716+rwittenberns@users.noreply.github.com>
2025-11-06 10:02:26 +0000

    Deploying to gh-pages from @ nl-design-system/lux@9f1771562c9552333459c8a4609239a415c1ddb0 🚀

---
+++ assets/form-field-checkbox.stories-Dsq0x03m.js
@@ -0,0 +1,395 @@
+var _a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k, _l, _m, _n, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y, _z, _A, _B, _C, _D, _E, _F, _G, _H, _I, _J, _K, _L, _M, _N, _O, _P;
+import { j as jsxRuntimeExports } from "./jsx-runtime-DnIIMar6.js";
+import { o as LuxFormFieldCheckbox } from "./index-BJfiFLmy.js";
+import { t as tokens } from "./index-DB79t5Cy.js";
+import { B as BADGES } from "./preview-DzEuGCsh.js";
+import { c as createDesignTokensStory } from "./createDesignTokensStory-HiB5Jh-Q.js";
+import { c as createVisualRegressionStory, V as VisualRegressionWrapper } from "./createVisualRegressionStory-Ckg-wQz5.js";
+import { m as meta$1 } from "./checkbox.stories-7csbV0QA.js";
+import { m as meta$2 } from "./form-field-description.stories-CdEiJ4_3.js";
+import { m as meta$3 } from "./form-field-error-message.stories-BVlmQ4_m.js";
+const VisualStates = () => /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label" }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label", checked: true }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label", description: "Description" }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx("h5", { className: "utrecht-heading-4", children: "Hover & Focus" }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "pseudo-hover-all", children: /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label" }) }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "pseudo-focus-all pseudo-focus-visible-all", children: /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label" }) }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx("h5", { className: "utrecht-heading-4", children: "Invalid" }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label", errorMessage: "Error Message", invalid: true }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label", description: "Description", errorMessage: "Error Message", invalid: true }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx("h5", { className: "utrecht-heading-4", children: "Disabled" }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label", disabled: true }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label", disabled: true, checked: true }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(LuxFormFieldCheckbox, { label: "Label", description: "Description", disabled: true })
+] });
+VisualStates.__docgenInfo = { "description": "", "methods": [], "displayName": "VisualStates" };
+const meta = {
+  title: "React Components/Form Field/Form Field Checkbox",
+  id: "react-components-form-field-form-field-checkbox",
+  component: LuxFormFieldCheckbox,
+  parameters: {
+    badges: [BADGES.WIP, BADGES.LATEST],
+    tokens,
+    tokensPrefix: "utrecht-form-field-checkbox"
+  },
+  argTypes: {
+    ...meta$1.argTypes,
+    description: {
+      ...meta$2.argTypes.children
+    },
+    appearance: {
+      ...meta$2.argTypes.appearance
+    },
+    distanced: {
+      ...meta$3.argTypes.distanced
+    },
+    disabled: {
+      type: "boolean"
+    },
+    withTarget: {
+      type: "boolean",
+      description: "Makes the whole inner part a click target."
+    },
+    errorMessage: {
+      if: {
+        arg: "invalid",
+        truthy: true
+      }
+    }
+  }
+};
+const Playground = {
+  name: "Playground",
+  args: {
+    label: "Label",
+    errorMessage: "ErrorMessage",
+    invalid: false,
+    appearance: void 0
+  },
+  parameters: {
+    docs: {
+      sourceState: "shown"
+    }
+  },
+  tags: ["!autodocs"]
+};
+const Invalid = {
+  name: "Invalid",
+  args: {
+    ...Playground.args,
+    invalid: true
+  }
+};
+const Disabled = {
+  name: "Disabled",
+  args: {
+    ...Playground.args,
+    disabled: true
+  }
+};
+const Hover = {
+  name: "Hover",
+  args: {
+    ...Playground.args
+  },
+  parameters: {
+    pseudo: {
+      hover: true
+    }
+  }
+};
+const Active = {
+  name: "Active",
+  args: {
+    ...Playground.args
+  },
+  parameters: {
+    pseudo: {
+      active: true
+    }
+  }
+};
+const Focus = {
+  name: "Focus",
+  args: {
+    ...Playground.args
+  },
+  parameters: {
+    pseudo: {
+      focus: true,
+      focusVisible: true
+    }
+  }
+};
+const WithDescription = {
+  name: "With Description",
+  args: {
+    ...Playground.args,
+    description: "Description"
+  }
+};
+const WithTarget = {
+  name: "With Target",
+  args: {
+    ...Playground.args,
+    withTarget: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn."
+      }
+    }
+  }
+};
+const HoverWithTarget = {
+  name: "Hover With Target",
+  args: {
+    ...Playground.args,
+    withTarget: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn."
+      }
+    },
+    pseudo: {
+      hover: true
+    }
+  }
+};
+const ActiveWithTarget = {
+  name: "Active With Target",
+  args: {
+    ...Playground.args,
+    withTarget: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn."
+      }
+    },
+    pseudo: {
+      active: true
+    }
+  }
+};
+const FocusWithTarget = {
+  name: "Fosus With Target",
+  args: {
+    ...Playground.args,
+    withTarget: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn."
+      }
+    },
+    pseudo: {
+      focus: true,
+      focusVisible: true
+    }
+  }
+};
+const withLongTexts = {
+  name: "With long texts",
+  args: {
+    ...Playground.args,
+    label: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit",
+    description: "Dolor ante id varius, aenean eu faucibus vitae malesuada. Viverra malesuada aliquam et placerat justo porta ipsum parturient.",
+    errorMessage: "Cursus nostra varius efficitur lobortis aliquam lectus bibendum",
+    invalid: true,
+    withTarget: true
+  }
+};
+const DesignTokens = createDesignTokensStory(meta);
+const Visual = createVisualRegressionStory(() => /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+  /* @__PURE__ */ jsxRuntimeExports.jsx("h4", { className: "utrecht-heading-3", children: "Light" }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(VisualRegressionWrapper, { className: `lux-theme--logius-light`, children: /* @__PURE__ */ jsxRuntimeExports.jsx(VisualStates, {}) }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx("h4", { className: "utrecht-heading-3", children: "Dark" }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(VisualRegressionWrapper, { className: `lux-theme--logius-dark`, children: /* @__PURE__ */ jsxRuntimeExports.jsx(VisualStates, {}) })
+] }));
+Visual.__docgenInfo = {
+  "description": "",
+  "methods": [],
+  "displayName": "Visual"
+};
+Playground.parameters = {
+  ...Playground.parameters,
+  docs: {
+    ...(_a = Playground.parameters) == null ? void 0 : _a.docs,
+    source: {
+      originalSource: "{\n  name: 'Playground',\n  args: {\n    label: 'Label',\n    errorMessage: 'ErrorMessage',\n    invalid: false,\n    appearance: undefined\n  },\n  parameters: {\n    docs: {\n      sourceState: 'shown'\n    }\n  },\n  tags: ['!autodocs']\n}",
+      ...(_c = (_b = Playground.parameters) == null ? void 0 : _b.docs) == null ? void 0 : _c.source
+    }
+  }
+};
+Invalid.parameters = {
+  ...Invalid.parameters,
+  docs: {
+    ...(_d = Invalid.parameters) == null ? void 0 : _d.docs,
+    source: {
+      originalSource: "{\n  name: 'Invalid',\n  args: {\n    ...Playground.args,\n    invalid: true\n  }\n}",
+      ...(_f = (_e = Invalid.parameters) == null ? void 0 : _e.docs) == null ? void 0 : _f.source
+    }
+  }
+};
+Disabled.parameters = {
+  ...Disabled.parameters,
+  docs: {
+    ...(_g = Disabled.parameters) == null ? void 0 : _g.docs,
+    source: {
+      originalSource: "{\n  name: 'Disabled',\n  args: {\n    ...Playground.args,\n    disabled: true\n  }\n}",
+      ...(_i = (_h = Disabled.parameters) == null ? void 0 : _h.docs) == null ? void 0 : _i.source
+    }
+  }
+};
+Hover.parameters = {
+  ...Hover.parameters,
+  docs: {
+    ...(_j = Hover.parameters) == null ? void 0 : _j.docs,
+    source: {
+      originalSource: "{\n  name: 'Hover',\n  args: {\n    ...Playground.args\n  },\n  parameters: {\n    pseudo: {\n      hover: true\n    }\n  }\n}",
+      ...(_l = (_k = Hover.parameters) == null ? void 0 : _k.docs) == null ? void 0 : _l.source
+    }
+  }
+};
+Active.parameters = {
+  ...Active.parameters,
+  docs: {
+    ...(_m = Active.parameters) == null ? void 0 : _m.docs,
+    source: {
+      originalSource: "{\n  name: 'Active',\n  args: {\n    ...Playground.args\n  },\n  parameters: {\n    pseudo: {\n      active: true\n    }\n  }\n}",
+      ...(_o = (_n = Active.parameters) == null ? void 0 : _n.docs) == null ? void 0 : _o.source
+    }
+  }
+};
+Focus.parameters = {
+  ...Focus.parameters,
+  docs: {
+    ...(_p = Focus.parameters) == null ? void 0 : _p.docs,
+    source: {
+      originalSource: "{\n  name: 'Focus',\n  args: {\n    ...Playground.args\n  },\n  parameters: {\n    pseudo: {\n      focus: true,\n      focusVisible: true\n    }\n  }\n}",
+      ...(_r = (_q = Focus.parameters) == null ? void 0 : _q.docs) == null ? void 0 : _r.source
+    }
+  }
+};
+WithDescription.parameters = {
+  ...WithDescription.parameters,
+  docs: {
+    ...(_s = WithDescription.parameters) == null ? void 0 : _s.docs,
+    source: {
+      originalSource: "{\n  name: 'With Description',\n  args: {\n    ...Playground.args,\n    description: 'Description'\n  }\n}",
+      ...(_u = (_t = WithDescription.parameters) == null ? void 0 : _t.docs) == null ? void 0 : _u.source
+    }
+  }
+};
+WithTarget.parameters = {
+  ...WithTarget.parameters,
+  docs: {
+    ...(_v = WithTarget.parameters) == null ? void 0 : _v.docs,
+    source: {
+      originalSource: "{\n  name: 'With Target',\n  args: {\n    ...Playground.args,\n    withTarget: true\n  },\n  parameters: {\n    docs: {\n      description: {\n        story: 'Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn.'\n      }\n    }\n  }\n}",
+      ...(_x = (_w = WithTarget.parameters) == null ? void 0 : _w.docs) == null ? void 0 : _x.source
+    }
+  }
+};
+HoverWithTarget.parameters = {
+  ...HoverWithTarget.parameters,
+  docs: {
+    ...(_y = HoverWithTarget.parameters) == null ? void 0 : _y.docs,
+    source: {
+      originalSource: "{\n  name: 'Hover With Target',\n  args: {\n    ...Playground.args,\n    withTarget: true\n  },\n  parameters: {\n    docs: {\n      description: {\n        story: 'Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn.'\n      }\n    },\n    pseudo: {\n      hover: true\n    }\n  }\n}",
+      ...(_A = (_z = HoverWithTarget.parameters) == null ? void 0 : _z.docs) == null ? void 0 : _A.source
+    }
+  }
+};
+ActiveWithTarget.parameters = {
+  ...ActiveWithTarget.parameters,
+  docs: {
+    ...(_B = ActiveWithTarget.parameters) == null ? void 0 : _B.docs,
+    source: {
+      originalSource: "{\n  name: 'Active With Target',\n  args: {\n    ...Playground.args,\n    withTarget: true\n  },\n  parameters: {\n    docs: {\n      description: {\n        story: 'Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn.'\n      }\n    },\n    pseudo: {\n      active: true\n    }\n  }\n}",
+      ...(_D = (_C = ActiveWithTarget.parameters) == null ? void 0 : _C.docs) == null ? void 0 : _D.source
+    }
+  }
+};
+FocusWithTarget.parameters = {
+  ...FocusWithTarget.parameters,
+  docs: {
+    ...(_E = FocusWithTarget.parameters) == null ? void 0 : _E.docs,
+    source: {
+      originalSource: "{\n  name: 'Fosus With Target',\n  args: {\n    ...Playground.args,\n    withTarget: true\n  },\n  parameters: {\n    docs: {\n      description: {\n        story: 'Met `withTarget` wordt het hele component (behalve de foutmelding) een klikdoel. _Let op:_ dit kan voor gebruikers onverwacht zijn.'\n      }\n    },\n    pseudo: {\n      focus: true,\n      focusVisible: true\n    }\n  }\n}",
+      ...(_G = (_F = FocusWithTarget.parameters) == null ? void 0 : _F.docs) == null ? void 0 : _G.source
+    }
+  }
+};
+withLongTexts.parameters = {
+  ...withLongTexts.parameters,
+  docs: {
+    ...(_H = withLongTexts.parameters) == null ? void 0 : _H.docs,
+    source: {
+      originalSource: "{\n  name: 'With long texts',\n  args: {\n    ...Playground.args,\n    label: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit',\n    description: 'Dolor ante id varius, aenean eu faucibus vitae malesuada. Viverra malesuada aliquam et placerat justo porta ipsum parturient.',\n    errorMessage: 'Cursus nostra varius efficitur lobortis aliquam lectus bibendum',\n    invalid: true,\n    withTarget: true\n  }\n}",
+      ...(_J = (_I = withLongTexts.parameters) == null ? void 0 : _I.docs) == null ? void 0 : _J.source
+    }
+  }
+};
+DesignTokens.parameters = {
+  ...DesignTokens.parameters,
+  docs: {
+    ...(_K = DesignTokens.parameters) == null ? void 0 : _K.docs,
+    source: {
+      originalSource: "createDesignTokensStory(meta)",
+      ...(_M = (_L = DesignTokens.parameters) == null ? void 0 : _L.docs) == null ? void 0 : _M.source
+    }
+  }
+};
+Visual.parameters = {
+  ...Visual.parameters,
+  docs: {
+    ...(_N = Visual.parameters) == null ? void 0 : _N.docs,
+    source: {
+      originalSource: 'createVisualRegressionStory(() => <>\n    <h4 className="utrecht-heading-3">Light</h4>\n    <VisualRegressionWrapper className={`lux-theme--logius-light`}>\n      <VisualStates />\n    </VisualRegressionWrapper>\n    <h4 className="utrecht-heading-3">Dark</h4>\n    <VisualRegressionWrapper className={`lux-theme--logius-dark`}>\n      <VisualStates />\n    </VisualRegressionWrapper>\n  </>)',
+      ...(_P = (_O = Visual.parameters) == null ? void 0 : _O.docs) == null ? void 0 : _P.source
+    }
+  }
+};
+const __namedExportsOrder = ["Playground", "Invalid", "Disabled", "Hover", "Active", "Focus", "WithDescription", "WithTarget", "HoverWithTarget", "ActiveWithTarget", "FocusWithTarget", "withLongTexts", "DesignTokens", "Visual"];
+const FormFieldCheckboxStories = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  Active,
+  ActiveWithTarget,
+  DesignTokens,
+  Disabled,
+  Focus,
+  FocusWithTarget,
+  Hover,
+  HoverWithTarget,
+  Invalid,
+  Playground,
+  Visual,
+  WithDescription,
+  WithTarget,
+  __namedExportsOrder,
+  default: meta,
+  withLongTexts
+}, Symbol.toStringTag, { value: "Module" }));
+export {
+  Active as A,
+  Disabled as D,
+  FormFieldCheckboxStories as F,
+  Hover as H,
+  Invalid as I,
+  Playground as P,
+  WithDescription as W,
+  Focus as a,
+  WithTarget as b,
+  ActiveWithTarget as c,
+  HoverWithTarget as d,
+  FocusWithTarget as e,
+  withLongTexts as w
+};
