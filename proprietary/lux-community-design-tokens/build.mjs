a1791ccca1ea1da23e8387b65d3f5505e8d5f1d4
Casper Ravenhorst <casper@sinedub.io>
2026-03-02 12:35:24 +0100

    feat: add lux-community-design-tokens package

---
+++ proprietary/lux-community-design-tokens/build.mjs
@@ -0,0 +1,64 @@
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+
+// Custom header to add generation date
+StyleDictionary.registerFileHeader({
+  name: 'nlds-rhc-header',
+  fileHeader: function (defaultMessage) {
+    return [...defaultMessage, `Generated on ${new Date().toUTCString()}`];
+  },
+});
+
+register(StyleDictionary, { excludeParentKeys: true });
+
+// Get the platforms config
+const getPlatformsConfig = (buildPath) => {
+  return {
+    web: {
+      transformGroup: 'tokens-studio',
+      transforms: ['name/kebab'],
+      buildPath,
+      options: {
+        fileHeader: 'nlds-rhc-header',
+        outputReferences: true,
+      },
+      files: [
+        {
+          destination: 'root.css',
+          format: 'css/variables',
+        },
+        {
+          destination: '_variables.scss',
+          format: 'scss/variables',
+        },
+      ],
+    },
+  };
+};
+
+// This will build the base tokens without the themes and without the overwrites
+async function buildBaseTokens() {
+  const config = getPlatformsConfig('dist/');
+  const StyleDictionaryBase = new StyleDictionary({
+    log: { verbosity: 'verbose' },
+    source: ['./src/figma.tokens.json'],
+    preprocessors: ['tokens-studio'],
+    platforms: {
+      ...config,
+    },
+  });
+  await StyleDictionaryBase.hasInitialized;
+
+  await StyleDictionaryBase.cleanAllPlatforms();
+  await StyleDictionaryBase.buildAllPlatforms();
+}
+
+async function build() {
+  try {
+    await buildBaseTokens();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+build();
