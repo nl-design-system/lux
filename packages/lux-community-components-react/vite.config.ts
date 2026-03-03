cf136ca6d1a56469684e651c191cb5899709acbb
Casper Ravenhorst <casper@sinedub.io>
2026-03-03 10:55:46 +0100

    feat: add community components package

---
+++ packages/lux-community-components-react/vite.config.ts
@@ -0,0 +1,33 @@
+/* eslint-env node */
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Components React',
+      // the proper extensions will be added
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+        manualChunks: undefined,
+      },
+    },
+    minify: false,
+  },
+  plugins: [dts(), react(), cssInjectedByJsPlugin({ topExecutionPriority: false })],
+});
