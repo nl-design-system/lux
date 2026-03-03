04cf359defc09faae36f5708d7e9c37f8f8d9da1
Casper Ravenhorst <casper@sinedub.io>
2026-03-03 14:18:18 +0100

    chore: replace jest with vitest

--- packages/lux-community-components-react/vite.config.ts
+++ packages/lux-community-components-react/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'Components React',
+      name: 'Community Components React',
       // the proper extensions will be added
       fileName: 'index',
     },
