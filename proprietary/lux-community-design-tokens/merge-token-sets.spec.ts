21b8105649af712a136d1b7499dc69d33e34a0f8
FinnWard <33222605+FinnWard@users.noreply.github.com>
2026-03-11 09:27:29 +0100

    fix:test

--- proprietary/lux-community-design-tokens/merge-token-sets.spec.ts
+++ proprietary/lux-community-design-tokens/merge-token-sets.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it } from 'vitest';
 import { mergeTokenSets } from './merge-token-sets.mjs';
 
 describe('mergeTokenSets', () => {
