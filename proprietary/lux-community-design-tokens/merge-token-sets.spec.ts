a2521c9020b61f07ded7cf84b7dc32d97c85fece
edblocker <eddy.harskamp@hybrit.org>
2026-08-11 15:31:50 +0200

    fix(token): Fix token studio metadata merge, adjust mergeTokenSets value and add unit tests

--- proprietary/lux-community-design-tokens/merge-token-sets.spec.ts
+++ proprietary/lux-community-design-tokens/merge-token-sets.spec.ts
@@ -3,7 +3,7 @@ import { mergeTokenSets } from './merge-token-sets.mjs';
 
 const defaultTheme = { $themes: [{ id: 'some' }, { id: 'stuff' }] };
 
-describe('mergeTokenSets', () => {
+describe('mergeTokenSets - basic behavior', () => {
   it('should not overwrite keys from the original', () => {
     const orig = { a: 'a', ...defaultTheme };
     const add = { a: 'b', ...defaultTheme };
@@ -40,5 +40,72 @@ describe('mergeTokenSets', () => {
     const [merged2] = mergeTokenSets(orig, add2);
 
     expect(merged2.$themes.find((x: { id: string }) => x.id === 'b')).not.toBeTruthy();
+  });
+});
+
+describe('mergeTokenSets - $metadata (tokenSetOrder)', () => {
+  it('should append the token set names of the addition', () => {
+    const orig = { $metadata: { tokenSetOrder: ['brand/color'] }, ...defaultTheme };
+    const add = { 'bwb/color': {}, ...defaultTheme };
+
+    const [merged] = mergeTokenSets(orig, add);
+
+    expect(merged.$metadata.tokenSetOrder).toStrictEqual(['brand/color', 'bwb/color']);
+  });
+
+  it('should not append keys prefixed with a $', () => {
+    const orig = { $metadata: { tokenSetOrder: ['brand/color'] }, ...defaultTheme };
+    const add = { 'bwb/color': {}, $metadata: { tokenSetOrder: ['bwb/other'] }, ...defaultTheme };
+
+    const [merged] = mergeTokenSets(orig, add);
+
+    expect(merged.$metadata.tokenSetOrder).toStrictEqual(['brand/color', 'bwb/color']);
+  });
+
+  it('should keep the token sets of the original first and in order', () => {
+    const orig = { $metadata: { tokenSetOrder: ['brand/color', 'brand/space'] }, ...defaultTheme };
+    const add = { 'bwb/color': {}, 'bwb/space': {}, ...defaultTheme };
+
+    const [merged] = mergeTokenSets(orig, add);
+
+    expect(merged.$metadata.tokenSetOrder).toStrictEqual(['brand/color', 'brand/space', 'bwb/color', 'bwb/space']);
+  });
+
+  it('should not add metadata when the original has none', () => {
+    const orig = { a: 'a', ...defaultTheme };
+    const add = { 'bwb/color': {}, ...defaultTheme };
+
+    const [merged] = mergeTokenSets(orig, add);
+
+    expect(merged.$metadata).toBeUndefined();
+  });
+
+  it('should not add a token set order when the metadata has none', () => {
+    const orig = { $metadata: { activeThemes: [] }, ...defaultTheme };
+    const add = { 'bwb/color': {}, ...defaultTheme };
+
+    const [merged] = mergeTokenSets(orig, add);
+
+    expect(merged.$metadata.tokenSetOrder).toBeUndefined();
+  });
+
+  it('should not mutate the metadata of the original', () => {
+    const orig = { $metadata: { tokenSetOrder: ['brand/color'] }, ...defaultTheme };
+    const add = { 'bwb/color': {}, ...defaultTheme };
+
+    mergeTokenSets(orig, add);
+
+    expect(orig.$metadata.tokenSetOrder).toStrictEqual(['brand/color']);
+  });
+
+  it('should not suffer from shallow copy residue', () => {
+    const orig = { $metadata: { tokenSetOrder: ['brand/color'] }, ...defaultTheme };
+    const add1 = { 'bwb/color': {}, ...defaultTheme };
+    const add2 = { 'voorbeeld/color': {}, ...defaultTheme };
+
+    mergeTokenSets(orig, add1);
+    const [merged2] = mergeTokenSets(orig, add2);
+
+    expect(merged2.$metadata.tokenSetOrder).toStrictEqual(['brand/color', 'voorbeeld/color']);
   });
 });
